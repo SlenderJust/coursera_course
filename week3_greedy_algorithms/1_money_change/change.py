b38fb977dda1289fc3af644e797ded2f25344251
SlenderJust <yamatotact@gmail.com>
2018-02-13 13:42:49 -0800

    finish week3 part1

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,9 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    return m//10 + m%10//5 + m%10%5
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+	m = int(sys.stdin.read())
+	print(get_change(m))
